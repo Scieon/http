c7f4b21ddf96292dcfc275c96a113156a59f607f
Anhkhoi Vu-Nguyen <anhkhoi.vunguyen@gmail.com>
2019-03-03 21:37:04 -0500

    parse args and finish GET

--- Httpfs.java
+++ Httpfs.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.*;
 
 public class Httpfs {
 
@@ -9,14 +9,42 @@ public class Httpfs {
     private final String HTTP_SUCCESS_200 = "HTTP/1.1 200 OK\r\n";
     private boolean debug = false;
     private int port = 999; // todo change to 8080 for submission
-    private String path = ".\\data\\";
+    private String path = "./data/";
 
     public static void main(String[] args) throws IOException {
         new Httpfs().run(args);
 
     }
 
+    private void parseArgs(String[] args) {
+        if (args == null || args.length == 0)
+            return;
+
+        List<String> argsList = new ArrayList<>(Arrays.asList(args));
+
+        if (args[0].equals("-v")) {
+            debug = true;
+        }
+
+        if (argsList.contains("-p")) {
+            port = Integer.parseInt(args[argsList.indexOf("-p") + 1]);
+        }
+
+        if (argsList.contains("-d")) {
+            String pathToDir = args[argsList.indexOf("-d") + 1];
+            path = path + pathToDir + "/";
+        }
+
+        if (debug) {
+            System.out.println("Server running on port " + port);
+            System.out.println("Path to directory: " + path);
+
+        }
+    }
+
     private void run(String[] args) throws IOException {
+        parseArgs(args);
+
         ServerSocket serverSocket = new ServerSocket(port);
         Socket socketConnection;
 
@@ -36,7 +64,9 @@ public class Httpfs {
 
             while (line != null && !line.isEmpty()) {
                 if (line.contains("GET") || line.contains("POST")) {
+//                    System.out.println(line);
                     urlPath = line.substring(line.indexOf("/") + 1, line.indexOf("HTTP/"));
+//                    System.out.println(urlPath);
                 }
                 line = reader.readLine();
             }
@@ -51,29 +81,32 @@ public class Httpfs {
         }
     }
 
-    // todo (All this does right now is print out files, do we need to return them??)
-    private void handleDefaultGet(PrintWriter out) {
-        // print list of files
-        File f = new File(path); // current directory
-
-        File[] files = f.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    if (debug) {
-                        System.out.println("Inside Directory: " + file);
-                    }
-                } else {
-                    String pathName = file.toString();
-                    String filename = pathName.substring(path.length());
-                    if (debug) {
-                        System.out.println("Found file: " + filename);
-                    }
-                }
+    private List<String> listFilesForFolder(final File folder, List<String> fileList) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry, fileList);
+            } else {
+                System.out.println(fileEntry.getName());
+                fileList.add(fileEntry.getName());
             }
         }
 
+        return fileList;
+    }
+
+    private void handleDefaultGet(PrintWriter out) {
+        File folder = new File(path);
+        List<String> fileList = new ArrayList<>();
+        fileList = listFilesForFolder(folder, fileList);
+
         out.print(HTTP_SUCCESS_200);
+
+        // Output all files found
+        for (String file : fileList) {
+//            System.out.println(file);
+            out.print("\r\n" + file);
+        }
+
         out.print("\r\n");
         out.println();
     }
@@ -85,15 +118,47 @@ public class Httpfs {
      * @param urlPath file path resource
      */
     private void handleGetPath(PrintWriter out, String urlPath) {
-        if (printFileContent(urlPath)) {
+        if (containsFile(urlPath)) {
             out.print(HTTP_SUCCESS_200);
+            printFileContent(out, urlPath);
             out.print("\r\n");
             out.println();
         } else {
             // File does not exist
             out.print(HTTP_ERROR_404);
+            out.print("\r\nFile not found");
             out.print("\r\n");
             out.println();
+        }
+    }
+
+
+    private void printFileContent(PrintWriter out, String filePath) {
+        try {
+            filePath = filePath.trim();
+            Scanner sc = new Scanner(new FileInputStream(path + filePath + ".txt"));
+            String line = sc.nextLine();
+
+            // Print first line
+            out.print("\r\n" + line);
+
+            if (debug) {
+                System.out.println(line);
+            }
+
+            // Print rest of lines
+            while (sc.hasNextLine()) {
+                line = sc.nextLine();
+
+                if (debug) {
+                    System.out.println(line);
+                }
+                out.print("\r\n" + line);
+            }
+        } catch (FileNotFoundException e) {
+            if (debug) {
+                System.out.println("File '" + filePath + "' was not found.");
+            }
         }
     }
 
@@ -103,16 +168,15 @@ public class Httpfs {
      * @param filePath - Path of url
      * @return true if file exists
      */
-    private boolean printFileContent(String filePath) {
+    private boolean containsFile(String filePath) {
         try {
             filePath = filePath.trim();
             Scanner sc = new Scanner(new FileInputStream(path + filePath + ".txt"));
-            String line = sc.nextLine();
 
-            while (sc.hasNextLine()) {
-                System.out.println(line);
-                line = sc.nextLine();
+            if (sc.hasNextLine()) {
+                return true;
             }
+
         } catch (FileNotFoundException e) {
             if (debug) {
                 System.out.println("File '" + filePath + "' was not found.");
@@ -121,5 +185,4 @@ public class Httpfs {
         }
         return true;
     }
-
 }
